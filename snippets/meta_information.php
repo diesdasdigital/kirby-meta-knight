0996987e74ca4fc06d3c82ca04b9f974548c03f7
Jakob Grommas <jakob@grommas-dietz.com>
2021-10-22 18:37:57 +0200

    Keep rel="alternate" conditional
    Since kirby->defaultLanguage() returns null by default, the page will crash without definition. This change keeps the whole rel="alternate" stuff conditional.

--- snippets/meta_information.php
+++ snippets/meta_information.php
@@ -55,10 +55,12 @@
 
 <?php // Alternate languages ?>
 
-<?php foreach ($kirby->languages() as $language): ?>
-  <link rel="alternate" hreflang="<?= strtolower(html($language->code())) ?>" href="<?= $page->url($language->code()) ?>">
-<?php endforeach; ?>
-<link rel="alternate" hreflang="x-default" href="<?= $page->url($kirby->defaultLanguage()->code()) ?>">
+<?php if ($defaultLanguage = $kirby->defaultLanguage()): ?>
+  <?php foreach ($kirby->languages() as $language): ?>
+    <link rel="alternate" hreflang="<?= strtolower(html($language->code())) ?>" href="<?= $page->url($language->code()) ?>">
+  <?php endforeach; ?>
+  <link rel="alternate" hreflang="x-default" href="<?= $page->url($defaultLanguage->code()) ?>">
+<?php endif; ?>
 
 <?php // Image ?>
 
